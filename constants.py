fc6347c63b7f9a00aaff7330c130a2bdfa86d138
“Benjamin <benjamin.drai@gmail.com>
2022-11-15 20:42:39 +0200

    Write some magic string into constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+BOXSCORE_KEY = "Boxscore flex flex-column"
+TEAM_NAME = "BoxscoreItem__TeamName h5"
+TEAM_TAB = "ResponsiveTable ResponsiveTable--fixed-left Boxscore flex flex-column"
+TABLE_BODY = "Table__TBODY"
+TABLE_CONTENT = "Table__TR Table__TR--sm Table__even"
+PLAYER = "Table Table--align-right Table--fixed Table--fixed-left"
+PLAYER_NAME = "AnchorLink truncate db external Boxscore__AthleteName"
+PLAYER_POSITION = "playerPosition pl2"
--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from player_game_stats import PlayerGameStats
 from stats import Stats
 from team_game_stats import TeamGameStats
+from constants import *
 import datetime
 
 
@@ -20,39 +21,44 @@ class Game:
         self.team2_stats = TeamGameStats(game_id)
 
     def get_team_tabs_html(self):
+        """Returns HTML object of the two teams' box score"""
         page = requests.get(self.url_stats)
         soup = BeautifulSoup(page.content, "html.parser")
-        teams_tab = soup.find("div", class_="StickyContainer").find_all("div", class_="Boxscore flex flex-column")
+        teams_tab = soup.find("div", class_="StickyContainer").find_all("div", class_=BOXSCORE_KEY)
         return teams_tab
 
     @staticmethod
     def get_players_html(tab):
-        return tab.find("table", class_="Table Table--align-right Table--fixed Table--fixed-left") \
-            .find("tbody", class_="Table__TBODY").find_all("tr", class_="Table__TR Table__TR--sm Table__even")
+        """Returns list of players' HTML object"""
+        return tab.find("table", class_=PLAYER).find("tbody", class_=TABLE_BODY).find_all("tr", class_=TABLE_CONTENT)
 
     @staticmethod
     def get_team_stats_data(tab):
+        """Returns list of players data's HTML object"""
         return tab.find("div", class_="Table__ScrollerWrapper relative overflow-hidden") \
             .find("div", class_="Table__Scroller").find("table", class_="Table Table--align-right") \
-            .find("tbody", class_="Table__TBODY").find_all("tr", class_="Table__TR Table__TR--sm Table__even")
+            .find("tbody", class_=TABLE_BODY).find_all("tr", class_=TABLE_CONTENT)
 
     @staticmethod
     def store_data_in_team(team, team_name, player_game_stats):
+        """Store data to the team specified in input"""
         team.set_team_name(team_name)
         team.add_player_game_stats(player_game_stats)
 
     @staticmethod
     def get_player_info(player_html):
-        player_name_div = player_html.find("a", "AnchorLink truncate db external Boxscore__AthleteName")
+        """Returns player id, full_name and position"""
+        player_name_div = player_html.find("a", PLAYER_NAME)
         if not player_name_div:
             return
         player_id, player_full_name = player_name_div["href"].split("/")[-2:]
         player_full_name = " ".join(map(lambda x: x.capitalize(), player_full_name.split("-")))
-        player_position = player_html.find("span", class_="playerPosition pl2").text
+        player_position = player_html.find("span", class_=PLAYER_POSITION).text
         return player_id, player_full_name, player_position
 
     @staticmethod
     def get_player_stats(player_data_html):
+        """Returns list of stats, either or not the player played the game (DNP), if not why (DNP Reason)."""
         dnp_div = player_data_html.find("td", class_="tc td Table__TD")
         is_dnp = False
         dnp_reason = None
@@ -66,11 +72,11 @@ class Game:
         return stats, is_dnp, dnp_reason
 
     def scrapping_stats(self):
+        """Main scrapping function calling all sub-functions"""
         teams_tab = self.get_team_tabs_html()
         for team_number, team_tab in enumerate(teams_tab):
-            team_name = team_tab.find("div", class_="BoxscoreItem__TeamName h5").text
-            tab = team_tab.find("div", class_="ResponsiveTable ResponsiveTable--fixed-left Boxscore flex flex-column") \
-                .find("div", class_="flex")
+            team_name = team_tab.find("div", class_=TEAM_NAME).text
+            tab = team_tab.find("div", class_=TEAM_TAB).find("div", class_="flex")
             players = self.get_players_html(tab)
             datas = self.get_team_stats_data(tab)
 
